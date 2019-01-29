5e43508b355b5e43cebfb41276aa01342f520f95
Avelino José Figueira Flores Figueira Flores <avesousa14@gmail.com>
2019-01-29 17:20:28 -0300

    Actualización de tabla para crear resumen de casos abiertos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var clave = "abc123";
 var arrayReclamosAbiertos = [];
+
 function clickear() {
     document.onkeyup = (function(e) {
         if (e.which == 13) {
@@ -43,29 +44,101 @@ function validacion(estado, texto, id) {
 
 function validarOpciones() {
     var opcion = document.getElementById("opcion").value;
-    document.getElementById("carga").style.display = "inline";
-    if (opcion != 3) {
+
+    if (opcion == 1) {
+        document.getElementById("casosAbiertos").style.display = "inline";
+        mostrarReclamosAbiertos();
+    } else if (opcion != 3) {
+        document.getElementById("carga").style.display = "inline";
         document.getElementById("nSap").style.display = "none";
         document.getElementById("reclamoSap").style.display = "none";
-    }
-    else {
+    } else {
+        document.getElementById("carga").style.display = "inline";
         document.getElementById("nSap").style.display = "inline";
         document.getElementById("reclamoSap").style.display = "inline";
 
     }
 } // Hay que modificar esta función para que discrimine entre las tres posibilidades, ya que en la carga con referencia la primer acción es mostrar el resumen de reclamos abiertos
 
-function mostrarReclamosAbiertos(){
+function mostrarReclamosAbiertos() {
+    var e = [
+        ["A001", "OTRO", "COSTA RICA", 4150.0, 14.0, "Mecánica", "Falta de limpieza", "Limpieza/desgrafitización", "10/09/2018", ""],
+        ["A008", "SAP", "SUCRE ANTONIO JOSE DE, MCAL.", 2917.0, 13.0, "Manual", "Rota", "Retirar", "12/09/2018", "Campana manual sin puerta retirar"],
+        ["A015", "MAIL", "DORREGO AV.", 2248.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "13/09/2018", "Imagen"],
+        ["A023", "MAIL", "MANSILLA LUCIO NORBERTO GENERAL", 3800.0, 14.0, "Mecánica", "Fuera de posición", "Reubicar", "17/09/2018", "Reubicar según padron en altura 3741"],
+        ["A033", "ENTE", "CASTAÑON", 3390.0, 8.0, "Mecánica", "Rota", "Reparar gancho", "16/09/2018", ""],
+        ["A042", "OTRO", "THAMES", 2078.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "19/09/2018", ""],
+        ["A048", "OTRO", "LA PAMPA", 3656.0, 13.0, "Mecánica", "Sin TAG", "Poner TAG", "19/09/2018", "Verificar N° de TAGS"],
+        ["A001", "OTRO", "COSTA RICA", 4150.0, 14.0, "Mecánica", "Falta de limpieza", "Limpieza/desgrafitización", "10/09/2018", ""],
+        ["A008", "SAP", "SUCRE ANTONIO JOSE DE, MCAL.", 2917.0, 13.0, "Manual", "Rota", "Retirar", "12/09/2018", "Campana manual sin puerta retirar"],
+        ["A015", "MAIL", "DORREGO AV.", 2248.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "13/09/2018", "Imagen"],
+        ["A023", "MAIL", "MANSILLA LUCIO NORBERTO GENERAL", 3800.0, 14.0, "Mecánica", "Fuera de posición", "Reubicar", "17/09/2018", "Reubicar según padron en altura 3741"],
+        ["A033", "ENTE", "CASTAÑON", 3390.0, 8.0, "Mecánica", "Rota", "Reparar gancho", "16/09/2018", ""],
+        ["A042", "OTRO", "THAMES", 2078.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "19/09/2018", ""],
+        ["A048", "OTRO", "LA PAMPA", 3656.0, 13.0, "Mecánica", "Sin TAG", "Poner TAG", "19/09/2018", "Verificar N° de TAGS"],
+        ["A001", "OTRO", "COSTA RICA", 4150.0, 14.0, "Mecánica", "Falta de limpieza", "Limpieza/desgrafitización", "10/09/2018", ""],
+        ["A008", "SAP", "SUCRE ANTONIO JOSE DE, MCAL.", 2917.0, 13.0, "Manual", "Rota", "Retirar", "12/09/2018", "Campana manual sin puerta retirar"],
+        ["A015", "MAIL", "DORREGO AV.", 2248.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "13/09/2018", "Imagen"],
+        ["A023", "MAIL", "MANSILLA LUCIO NORBERTO GENERAL", 3800.0, 14.0, "Mecánica", "Fuera de posición", "Reubicar", "17/09/2018", "Reubicar según padron en altura 3741"],
+        ["A033", "ENTE", "CASTAÑON", 3390.0, 8.0, "Mecánica", "Rota", "Reparar gancho", "16/09/2018", ""],
+        ["A042", "OTRO", "THAMES", 2078.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "19/09/2018", ""],
+        ["A048", "OTRO", "LA PAMPA", 3656.0, 13.0, "Mecánica", "Sin TAG", "Poner TAG", "19/09/2018", "Verificar N° de TAGS"],
+        ["A001", "OTRO", "COSTA RICA", 4150.0, 14.0, "Mecánica", "Falta de limpieza", "Limpieza/desgrafitización", "10/09/2018", ""],
+        ["A008", "SAP", "SUCRE ANTONIO JOSE DE, MCAL.", 2917.0, 13.0, "Manual", "Rota", "Retirar", "12/09/2018", "Campana manual sin puerta retirar"],
+        ["A015", "MAIL", "DORREGO AV.", 2248.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "13/09/2018", "Imagen"],
+        ["A023", "MAIL", "MANSILLA LUCIO NORBERTO GENERAL", 3800.0, 14.0, "Mecánica", "Fuera de posición", "Reubicar", "17/09/2018", "Reubicar según padron en altura 3741"],
+        ["A033", "ENTE", "CASTAÑON", 3390.0, 8.0, "Mecánica", "Rota", "Reparar gancho", "16/09/2018", ""],
+        ["A042", "OTRO", "THAMES", 2078.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "19/09/2018", ""],
+        ["A048", "OTRO", "LA PAMPA", 3656.0, 13.0, "Mecánica", "Sin TAG", "Poner TAG", "19/09/2018", "Verificar N° de TAGS"],
+        ["A001", "OTRO", "COSTA RICA", 4150.0, 14.0, "Mecánica", "Falta de limpieza", "Limpieza/desgrafitización", "10/09/2018", ""],
+        ["A008", "SAP", "SUCRE ANTONIO JOSE DE, MCAL.", 2917.0, 13.0, "Manual", "Rota", "Retirar", "12/09/2018", "Campana manual sin puerta retirar"],
+        ["A015", "MAIL", "DORREGO AV.", 2248.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "13/09/2018", "Imagen"],
+        ["A023", "MAIL", "MANSILLA LUCIO NORBERTO GENERAL", 3800.0, 14.0, "Mecánica", "Fuera de posición", "Reubicar", "17/09/2018", "Reubicar según padron en altura 3741"],
+        ["A033", "ENTE", "CASTAÑON", 3390.0, 8.0, "Mecánica", "Rota", "Reparar gancho", "16/09/2018", ""],
+        ["A042", "OTRO", "THAMES", 2078.0, 14.0, "Mecánica", "Volcada", "Poner en posición", "19/09/2018", ""],
+        ["A048", "OTRO", "LA PAMPA", 3656.0, 13.0, "Mecánica", "Sin TAG", "Poner TAG", "19/09/2018", "Verificar N° de TAGS"]
+    ];
+    console.log(e);
+    var tabla = document.getElementById("casos");
+    console.log(tabla);
+    console.log(0 < e.length);
+    for (var i = 0; i < e.length; i++) {
+        var fila = document.createElement("TR"); // Creo una fila nueva
+        console.log("ENTRO EN EL PRIMER FOR");
+        console.log(i);
+        fila.setAttribute("id", ("valor" + i)); // Le envío un id, para identificar cada fila.
+        for (var j = 0; j < e[i].length; j++) {
+            var columna = document.createElement("TD"); // creo una columna nueva
+            var textoColumna = document.createTextNode(e[i][j]); //Creo el texto que va tener la columna correspondiente
+            columna.appendChild(textoColumna); // Acá inserto a la columna el texto correspondiente.
+            fila.appendChild(columna); //Acá inserto la columna a la fila.
+        }
+        var boton = document.createElement("TD"); //creo otra columna para definir el botón de actualizar
+        boton.innerHTML = '<button onclick="actualizar("' + i + ')">Actualizar</button>'; //Coloca el código html para generar un botón
+        fila.appendChild(boton); //Acá inserto el botón a la fila.
+        tabla.appendChild(fila); //Acá inserto la fila a la tabla.
+    }
+    /*google.script.run.withSuccesHandler(function(e){
+     arrayReclamosAbiertos = e;
 
-
-google.script.run.withSuccesHandler(function(e){
- arrayReclamosAbiertos = e;
-
- for(var i;i < e.length; i++){
- 
- for(var j; j < e[i].length;j++)
-
- }
-}).reclamosAbiertos();
+     for(var i;i < e.length; i++){
+        var fila = document.createElement("TR"); // Creo una fila nueva
+        fila.setAttribute("id",("valor"+i)); // Le envío un id, para identificar cada fila.
+        for(var j; j < e[i].length;j++){
+            var columna = document.createElement("TD"); // creo una columna nueva
+            var textoColumna = document.createTextNode(e[i][j]); //Creo el texto que va tener la columna correspondiente
+            columna.appendChild(textoColumna); // Acá inserto a la columna el texto correspondiente.
+            fila.appendChild(columna); //Acá inserto la columna a la fila.
+        }
+        var boton = document.createElement("TD"); //creo otra columna para definir el botón de actualizar
+        boton.innerHTML = '<button onclick="actualizar("'+i+')">Actualizar</button>'; //Coloca el código html para generar un botón
+        fila.appendChild(boton); //Acá inserto el botón a la fila.
+        tabla.appendChild(fila); //Acá inserto la fila a la tabla.
+    }
+    }).reclamosAbiertos();*/
 
 } // Obtiene los reclamos abiertos desde la planilla de Coldan y los muestra en pantalla.
+
+function actualizar(i) {
+    alert("NO ESTÁ REALIZADA AÚN PERO EL ID ES: " + i)
+} //Se realiza para actualizar el caso correspondiente.
